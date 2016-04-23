a4b8c4d1d4b70cd6ca8bc3da685fe3ec813d6f26
Remo Regli <remo.regli@gmail.com>
2016-04-23 12:11:35 +0200

    remove 'Kernel.' extract get_number method

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,17 +1,29 @@
-OPERATORS = ['+', '-', '*', '/']
+INTRO_MSG = "Welcome to this calculator app".freeze
+OPERATORS = ['+', '-', '*', '/'].freeze
 
-Kernel.puts("- Calculator -")
-Kernel.puts()
+def valid_number?(number)
+  true if /^\d*\.?\d*$/.match number
+end
 
-Kernel.puts("Please enter number 1: ")
-number1 = Kernel.gets().to_i()
-Kernel.puts("Please enter number 2: ")
-number2 = Kernel.gets().to_i()
-Kernel.puts()
+def get_number
+  loop do
+    puts "Please enter number:"
+    number = gets.chomp
+    return number.to_f if valid_number? number
+    puts "Number not valid! (1234 / 12.34):"
+  end
+end
 
-Kernel.puts("Choose operation; + [0], - [1], * [2], / [3]:")
-operator = Kernel.gets().to_i
+system('clear screen')
 
+puts INTRO_MSG
+puts
+
+number1 = get_number
+number2 = get_number
+
+puts "Choose operation; + [0], - [1], * [2], / [3]:"
+operator = gets.to_i
 
 result = number1.send(OPERATORS[operator], number2)
-Kernel.puts(result)
+puts "Result: #{result}"
