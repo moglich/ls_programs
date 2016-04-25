8e3571fe40979ac608b9857e16f87aba9abf08b3
Remo Regli <remo.regli@gmail.com>
2016-04-25 22:52:06 +0200

    update formating of calculations

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -1,7 +1,7 @@
 def calculate_monthy_payment(loan_amount, time_in_month, interest_rate)
-  interest_rate = (interest_rate.to_f/100)/12
-  (loan_amount * (interest_rate * (1 + interest_rate)**time_in_month)/((1 + interest_rate)**time_in_month - 1))
-end 
+  interest_rate = ((interest_rate.to_f / 100) / 12)
+  (loan_amount * (interest_rate * (1 + interest_rate)**time_in_month)) / ((1 + interest_rate)**time_in_month - 1)
+end
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -20,4 +20,4 @@ time_in_month = loan_duration * 12
 
 monthly_payment = calculate_monthy_payment(loan_amount, time_in_month, interest_rate)
 
-prompt "Monthly payment = #{format('%02.2f', monthly_payment)}" 
+prompt "Monthly payment = #{format('%02.2f', monthly_payment)}"
